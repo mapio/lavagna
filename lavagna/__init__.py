94ce28fee2e02633b7a0409da96e54c2c5616b33
Massimo Santini <santini@dsi.unimi.it>
2012-10-21 04:17:23 +0200

    Fixes and tweaks
    --HG--
    rename : bin/student => bin/clear_seat
    rename : bin/student => bin/seat

--- lavagna/__init__.py
+++ lavagna/__init__.py
@@ -6,57 +6,64 @@ from datetime import datetime
 from flask import Flask, render_template, request, Response, abort, session, redirect, url_for
 from jinja2.exceptions import TemplateNotFound
 
-from db import seat, studentat, question, answer, messages
+import db
 
 app = Flask( __name__ )
 app.secret_key = '899BFF36-68AF-47B8-A1F0-A764A96A90CF'	# should be in a conf file!
 app.config[ 'SESSION_COOKIE_HTTPONLY' ] = False
 
-@app.route( '/post/answer', methods = [ 'POST' ] )
-def post_answer():
-	f = request.form
-	if not 'answer' in f: abort( 500 )
-	if not 'kind' in f: abort( 500 )
-	if not 'destination' in f: abort( 500 )
-	answer( f[ 'answer' ], f[ 'kind' ], f[ 'destination' ] )
-	return ''
-
 @app.route( '/stream/<channel>' )
 def stream( channel ):
 	def event_stream():
-		for data in messages( channel ):
+		for data in db.messages( channel ):
 			for line in data.split( '\n' ):
 				yield 'data: {0}\n'.format( line )
 			yield '\n'
 	return Response( event_stream(), mimetype = 'text/event-stream' )
 
-@app.route( '/post/question', methods = [ 'POST' ] )
-def post_question():
-	if 'location' in request.form: # this happens when the teacher deletes questions
-		location = request.form[ 'location' ]
-	elif 'location' in session: 
-		location = session[ 'location' ]
-	else: abort( 404 )
-	if not studentat( location ): abort( 500 )
-	if not 'question' in request.form: abort( 500 )
-	question( request.form[ 'question' ], location )
+@app.route( '/post/answer', methods = [ 'POST' ] )
+def answer():
+	f = request.form
+	if not 'answer' in f: abort( 500 )
+	if not 'kind' in f: abort( 500 )
+	if not 'destination' in f: abort( 500 )
+	db.answer( f[ 'answer' ], f[ 'kind' ], f[ 'destination' ] )
 	return ''
+
+@app.route( '/post/question', methods = [ 'POST' ] )
+def question():
+	if not 'location' in session: abort( 404 )
+	if not db.studentat( session[ 'location' ] ): abort( 500 )
+	if not 'question' in request.form: abort( 500 )
+	db.question( request.form[ 'question' ], session[ 'location' ] )
+	return ''
+
+@app.route( '/post/clear_questions', methods = [ 'POST' ] )
+def clear_questions():
+	if not 'location' in request.form: abort( 500 )
+	if not db.studentat( request.form[ 'location' ] ): abort( 500 )
+	db.clear_questions( request.form[ 'location' ] )
+	return ''
+
+@app.route( '/post/clear_seat', methods = [ 'POST' ] )
+def clear_seat():
+	if not 'location' in request.form: abort( 500 )
+	db.clear_seat( request.form[ 'location' ] )
+	return 'OK'
+
+@app.route( '/seat/<student>/<location>' )	
+def seat( student, location ):
+	session[ 'location' ] = location
+	db.seat( student, location )
+	return redirect( url_for( 'student' ) )
 
 @app.route( '/' )
 def student():
 	if not 'location' in session: abort( 404 )
 	location = session[ 'location' ]
-	student = studentat( location )
-	if not student: abort( 500 )
+	student = db.studentat( location )
+	#if not student: abort( 500 )
 	return render_template( 'student.html', student = student, location = location )
-
-@app.route( '/seat/<location>' ) # this empties the seat (and deletes questions)
-@app.route( '/seat/<student>/<location>' )
-def set_session( location, student = None ):
-	session[ 'location' ] = location
-	seat( student, location )
-	if student: return redirect( url_for( 'student' ) )
-	else: return ''
 
 @app.route( '/t/<rooms>' )
 def teacher( rooms ):
--- lavagna/db.py
+++ lavagna/db.py
@@ -26,37 +26,30 @@ def studentat( location ):
 	return red.get( 'studentat:{0}'.format( location ) )
 
 def seat( student, location ):
-	if ( student ):
-		if studentat( location ) == student: return
-		red.set( 'studentat:{0}'.format( location ), student )
-		data = dumps( {
-			'event': 'seat',
-			'student': student,
-			'location': location,
-		} )
-		clear_questions( location )
-		red.publish( 'teacher', data )
-		red.rpush( 'persist:teacher', data )
-	else: # only the teacher can seat empty students
-		student = studentat( location )
-		if ( not student ): return
-		data = dumps( {
-			'event': 'seat',
-			'student': student,
-			'location': location,
-		} )
-		red.delete( 'studentat:{0}'.format( location ) )
-		clear_questions( location )
-		red.lrem( 'persist:teacher', 1, data )
+	if studentat( location ) == student: return
+	red.set( 'studentat:{0}'.format( location ), student )
+	data = dumps( {
+		'event': 'seat',
+		'student': student,
+		'location': location,
+	} )
+	clear_questions( location )
+	red.publish( 'teacher', data )
+	red.rpush( 'persist:teacher', data )
 
-def clear_questions( location ):
-	red.srem( 'persist:teacher:questions', location )
-	red.ltrim( 'persist:teacher:question:{0}'.format( location ), 1, 0 ) # start > end => delete
+def clear_seat( location ):
+	student = studentat( location )
+	if ( not student ): return
+	data = dumps( {
+		'event': 'seat',
+		'student': student,
+		'location': location,
+	} )
+	red.delete( 'studentat:{0}'.format( location ) )
+	clear_questions( location )
+	red.lrem( 'persist:teacher', 1, data )
 
 def question( question, location ):
-	if ( not question ): # only the teacher can post empty questions
-		clear_questions( location )
-		return
 	data = dumps( {
 		'event': 'question',
 		'student': studentat( location ),
@@ -67,6 +60,10 @@ def question( question, location ):
 	red.publish( 'teacher', data )
 	red.sadd( 'persist:teacher:questions', location )
 	red.rpush( 'persist:teacher:question:{0}'.format( location ), data )
+
+def clear_questions( location ):
+	red.srem( 'persist:teacher:questions', location )
+	red.ltrim( 'persist:teacher:question:{0}'.format( location ), 1, 0 )
 
 def answer( answer, kind, destination ):
 	data = dumps( {
